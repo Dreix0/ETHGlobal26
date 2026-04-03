6e3708b1663407f6635066a000c7411a8cc114d5
Dreix0 <quentin.denolle2001@gmail.com>
2026-04-03 23:06:33 +0200

    read wallet

---
+++ cannes26/app/components/ReadWallet.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { HDNodeWallet, Wallet } from "ethers";
+import { invoke } from "@tauri-apps/api/core";
+
+export default function ReadWallet() {
+  const [wallet, setWallet] = useState<HDNodeWallet | Wallet>();
+
+  async function connect(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    // Remplace ce chemin par un emplacement sur ton disque ou USB
+    const filePath = "C:/Users/quent/Desktop/Test/mon_texte.txt";
+
+    try {
+      const data = await invoke("read_text_from_file", {filePath}) as string;
+      const encryptedWallet = JSON.parse(data).wallet as string;
+
+      const decryptedWallet = Wallet.fromEncryptedJsonSync(encryptedWallet, e.target.password.value);
+      setWallet(decryptedWallet);
+
+    } catch (err) {
+      console.log("Erreur : " + err);
+    }
+  }
+
+  return (
+    <main>
+      <div>
+          <h3>Saisissez un mot de passe</h3>
+          <form onSubmit={connect}>
+              <input type="password" name="password" placeholder='Mot de passe'/>
+              <button type="submit" >Suivant</button>
+          </form>
+      </div>
+      <br />
+      <p>Address: {wallet ? wallet.address : "No wallet generated"}</p>
+      <p>Private Key: {wallet ? wallet.privateKey : "No wallet generated"}</p>
+      <p>Mnemonic: {wallet ? wallet.mnemonic?.phrase : "No wallet generated"}</p>
+    </main>
+  );
+}
--- cannes26/app/page.tsx
+++ cannes26/app/page.tsx
@@ -3,13 +3,20 @@
 import styles from "./styles/page.module.css";
 import CreateWallet from "./components/CreateWallet";
 import ImportWallet from "./components/ImportWallet";
+import ReadWallet from "./components/ReadWallet";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Create wallet</h1>
         <CreateWallet />
+        <hr />
+        <h1>Import wallet</h1>
         <ImportWallet />
+        <hr />
+        <h1>Read wallet</h1>
+        <ReadWallet />
       </main>
     </div>
   );
